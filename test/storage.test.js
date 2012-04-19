b013c1bcb8a13dc3875a2efaf995c8188716daba
rogeriopvl <rogeriopvl@gmail.com>
2012-04-19 23:23:27 +0100

    Added real tests to Storate module. Fixed read() method to parse the json.

--- test/storage.test.js
+++ test/storage.test.js
@@ -1,3 +1,33 @@
 var Storage = require('../lib/storage');
+var path = require('path');
 
+module.exports = {
 
+    'test CreateFileOnStartup': function(beforeExit, assert){
+        var testFile = '/Users/rogeriopvl/Documents/projects/nodo/test.stor';
+        var storage = new Storage(testFile);
+        assert.ok(path.existsSync(testFile));
+    },
+
+    'test ReadFileOnStartup': function(beforeExit, assert){
+        var testFile = '/Users/rogeriopvl/Documents/projects/nodo/test.stor';
+        var storage = new Storage(testFile);
+        testTasks = [
+            {
+                id: 1,
+                message: 'testing message 1'
+            },
+            {
+                id: 2,
+                message: 'testing message 2'
+            }
+        ];
+        storage.tasks = testTasks;
+        storage.save();
+
+        storage = undefined;
+        var storage2 = new Storage(testFile);
+        assert.eql(storage2.tasks, testTasks);
+    }
+
+};
