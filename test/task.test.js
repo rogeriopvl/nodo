abff9df65a12b57779efa180d07353ed5daa1e8a
rogeriopvl <rogeriopvl@gmail.com>
2012-05-09 18:12:01 +0100

    Added tests to task. Fixed found errors.

---
+++ test/task.test.js
@@ -0,0 +1,23 @@
+var database = require('../lib/database');
+var Task = require('../lib/task');
+
+module.exports = {
+
+    setUp: function(callback){
+        this.task = new Task();
+        callback();
+    },
+
+    testAdd: function(test){
+        var newTask = {list: 'Inbox', name: 'testing', dueDate: 0};
+        this.task.add(newTask, function(err, result){
+            test.ifError(err);
+            test.ok(result.lastId);
+            test.done();
+        });
+    },
+
+    testGet: function(test){
+        test.done();
+    }
+};
