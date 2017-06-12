1bd2129a3d4c082d40208c78751c4e3c079120af
rogeriopvl <rogeriopvl@gmail.com>
2017-06-12 10:30:33 +0100

    chore: remove grunt dependency
    All the tasks that are being done by grunt can be easily replaced by a
    npm script. This commit removes grunt dependencies and clears the test
    so that it can be rewritten in tape.

--- test/task.test.js
+++ test/task.test.js
@@ -1,23 +1,8 @@
-var database = require('../lib/database');
-var Task = require('../lib/task');
+var test = require('tape');
+// var database = require('../lib/database');
+// var Task = require('../lib/task');
 
-module.exports = {
-
-    setUp: function(callback){
-        this.task = new Task();
-        callback();
-    },
-
-    testAdd: function(test){
-        var newTask = {list: 'inbox', name: 'testing', dueDate: 0};
-        this.task.add(newTask, function(err, result){
-            test.ifError(err);
-            test.ok(result.lastId);
-            test.done();
-        });
-    },
-
-    testGet: function(test){
-        test.done();
-    }
-};
+test('it works', function (t) {
+  t.ok(true);
+  t.end();
+});
